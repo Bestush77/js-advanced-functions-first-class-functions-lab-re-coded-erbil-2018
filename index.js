fb74765a643b75be10faa24704014b5f0a2ec466
Bestun Gardi <bestun95@yahoo.com>
2018-07-25 12:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) { 
+  let newArray=[array[0], array[1]];
+  
+  return newArray;}
+  
+  
+  const returnLastTwoDrivers = function (array) {
+    let newArray = [array[(array.length-2)], array[(array.length-1)]]
+    return newArray;
+  }
+  
+  const selectingDrivers = function (array) {
+    let newArray = [[function (array) { 
+  let newArray=[array[0], array[1]];
+  
+  return newArray;}],[]];
+  }
